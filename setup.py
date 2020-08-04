962b598a5e6e4b94bd27c44d969be9558eeaeee3
Xiaoyi Chu <chuxiaoyi@papayamobile.cn>
2020-08-04 14:54:48 +0800

    bugfix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 # ---------------------------------------------
 from setuptools import setup, find_packages
 
-GFICLEE_VERSION = '2020.8.4'
+GFICLEE_VERSION = '2020.8.4.a'
 
 setup(
     name='cfastproject',
